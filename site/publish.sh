82376fd1f1b91f014d0c59854d87ab3502006b7b
Hervé Le Meur <lemeurherve.jenkins@gmail.com>
2023-10-17 15:29:59 +0200

    original rsync from local www2 to pkg VM /tmp/www/updates.jenkins.io temporary folder

--- site/publish.sh
+++ site/publish.sh
@@ -31,6 +31,7 @@ export PATH=.:$PATH
 # # delete old sites
 chmod -R a+r "${ROOT_FOLDER}"/www2
 # rsync -acvz www2/ --exclude=/updates --delete ${RSYNC_USER}@${UPDATES_SITE}:/var/www/${UPDATES_SITE}
+rsync -acvz "${ROOT_FOLDER}"/www2/ --exclude=/updates --delete ${RSYNC_USER}@${UPDATES_SITE}:/tmp/www/${UPDATES_SITE}
 
 # Remove simlinks
 find "${ROOT_FOLDER}"/www2 -type l -delete
